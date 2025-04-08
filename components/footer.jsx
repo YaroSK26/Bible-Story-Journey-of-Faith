1d7cad5e9ef9bcbecdbaf2b8642f0594b5dca1c0
jaxo <jaroba0@gmail.com>
2025-04-08 19:40:40 +0200

    zmen a domeny porfolia

--- components/footer.jsx
+++ components/footer.jsx
@@ -1,31 +1,29 @@
-
 const footer = () => {
   return (
     <footer className=" z-0 sm:fixed relative bottom-0 w-full text-center px-2 py-4  bg-gray-900 flex flex-col gap-1">
-  <p className="text-white">
-    Made with ❤️ by{" "}
-    <a
-      href="https://www.jaroslav.website/"
-      target="_blank"
-      className="underline"
-    >
-      Jaroslav Barabáš
-    </a>
-  </p>
+      <p className="text-white">
+        Made with ❤️ by{" "}
+        <a
+          href="https://www.jaroslav-portfolio.eu/"
+          target="_blank"
+          className="underline"
+        >
+          Jaroslav Barabáš
+        </a>
+      </p>
 
-  <p>
-    Also you can check my other christian project{" "}
-    <a
-      href="https://trust-god.vercel.app/"
-      target="_blank"
-      className="underline"
-    >
-      TRUST
-    </a>
-  </p>
-</footer>
+      <p>
+        Also you can check my other christian project{" "}
+        <a
+          href="https://trust-god.vercel.app/"
+          target="_blank"
+          className="underline"
+        >
+          TRUST
+        </a>
+      </p>
+    </footer>
+  );
+};
 
-  )
-}
-
-export default footer
+export default footer;
